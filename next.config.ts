68bc3971020f3a3c4fd23b42ba8e586e38eddb19
Gi <gi@macbookpro.home>
2025-10-27 15:22:02 +0100

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    basePath: '/russian-orthodox-church-vancouver',
     // output: 'export', // <=== enables static exports
     reactStrictMode: true,
 };
